b3b6a2e0ddaaf3b653c5c18731b0bd5737af9ba3
nandohidayat <mnandohidayat@gmail.com>
2019-09-26 21:22:25 +0700

    add base component that can be accessed from anywhere

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
     <h1>Event Listing</h1>
-
-    <router-link :to="{ name: 'event-show' , params: { id: '1' } }">Show Event #1</router-link>|
+    <EventCard />
+    <BaseIcon />
   </div>
 </template>
 
 <script>
-export default {};
+import EventCard from "@/components/EventCard.vue";
+export default {
+  components: {
+    EventCard
+  }
+};
 </script>
 
 <style>
