34be1163a9f128ed30650c21d0f45e42ee848f8a
nandohidayat <mnandohidayat@gmail.com>
2019-10-01 14:56:20 +0700

    add BaseInput

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <input type="text" :value="value" @input="updateValue" v-bind="$attrs" />
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      default: ""
+    },
+    value: [String, Number]
+  },
+  methods: {
+    updateValue() {
+      this.$emit("input", event.target.value);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
